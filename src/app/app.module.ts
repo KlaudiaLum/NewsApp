c583ae0b6f778e15ba7525038264c2fdb7583779
KlaudiaLum <klaudia.wroblewska@luminator.com>
2024-04-04 18:06:37 +0200

    added function for titile with app but with default lon and lat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TitileComponent } from './titile/titile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
