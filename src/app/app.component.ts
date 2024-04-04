fc4167f48cf0b805cc7a850dd78df4d0720c7114
KlaudiaLum <klaudia.wroblewska@luminator.com>
2024-04-04 15:50:06 +0200

    setup primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  template: `
-  <p> NewsApp </p>`,
-  styleUrls: ['./app.component.scss']
+  template: ` <p>NewsApp</p>`,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'NewsApp';
