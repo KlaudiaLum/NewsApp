bfcacfbf092afe9a569fc17018407fd453852ad9
KlaudiaLum <158457058+KlaudiaLum@users.noreply.github.com>
2024-04-09 10:25:26 +0200

    Libpis (#5)
    * try npm
    
    * build: fix version @angular-devkit/build-angular
    
    * try connectet libpis
    
    * fixed problems from piplines
    
    ---------
    
    Co-authored-by: Gustav Blomqvist <gustav.blomqvist@luminatorhost.onmicrosoft.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { environment } from '../environment/environment';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <p> NewsApp </p>`,
-  styleUrls: ['./app.component.scss']
+  template: ` <p>NewsApp</p>`,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'NewsApp';
+  mqttConfig = environment.mqtt;
+  state: any;
+
+  ngOnInit(): void {
+    this.initConnection();
+  }
+
+  // connectet libpis with mqtt broker
+  initConnection() {
+    if (window.luminator && window.luminator.pis) {
+      window.luminator.pis.init(this.mqttConfig);
+  
+      window.luminator.pis.client.updates().subscribe({
+        next: (state: any) => {
+          if (state) {
+            console.log('LibPis ', state);
+          } else {
+            console.log('Waiting for data...');
+          }
+        },
+        error: (error: any) => {
+          console.error('Error occurred while fetching data:', error);
+        },
+      });
+    } else {
+      console.error('luminator or pis is not defined');
+    }
+  }
+  
 }
