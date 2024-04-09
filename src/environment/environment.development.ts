0662c23605a997adb051e238bf598b727f660987
Gustav Blomqvist <158456476+GustavBlomqvist@users.noreply.github.com>
2024-04-09 16:00:43 +0200

    feat: add type of news (#10)
    * feat: add type of news
    
    * fix: connection with libpis
    
    ---------
    
    Co-authored-by: Gustav Blomqvist <gustav.blomqvist@luminatorhost.onmicrosoft.com>
    Co-authored-by: KlaudiaLum <klaudia.wroblewska@luminator.com>

--- src/environment/environment.development.ts
+++ src/environment/environment.development.ts
@@ -1,9 +1,12 @@
 export const environment = {
+  production: false,
+  apiBasePath:
+    'https://backend30b1a612.wittyriver-52710109.northeurope.azurecontainerapps.io/dynamic-news/api/news/current?lon=14.595295&lat=56.920602&blacklistSources=svt',
   mqtt: {
     hostname: 'ltg-mss-dev-mosquitto-01.northeurope.cloudapp.azure.com',
     port: 1883,
     username: 'ltg-mss-dev-mosquitto-01',
     password: 'yjURZS2DbaQKbX5',
-    displayId: '1'
+    displayId: '1',
   },
 };
