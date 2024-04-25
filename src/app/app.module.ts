25082752acbeeea1c4f595af3cb21c4eda8eda1c
KlaudiaLum <158457058+KlaudiaLum@users.noreply.github.com>
2024-04-25 11:16:00 +0200

    Feat/layout (#15)
    * added function that show only current stop
    
    print news on the app and also added images
    
     write type of data
    
    move function to the stop list copomonent , make code more easier and more readable
    
     change ng-templet to ng -container  , added throttleTime method , and change model fro NewData
    
    try fix porblem with version of rxjs
    
    try fix porblem with version of rxjs
    
    try fix porblem with version of rxjs
    
    change verion primeng
    
    remove string
    
    try fix some code
    
    try fix some code
    
    try fix some code
    
    try fix some code
    
    * build: add @msetsuite/libpis
    
    This also fixes the version of rxjs and primeng.
    
    * creat layout and added url to qr code
    
    * Added function to show only one new message and after 20s show another one, and add background colour depending on message type.
    
    * change name of function to more readable
    
    * fix: remove duplicate dependencies
    
    * fix:  uninstall primeflex primeicons primeng
    
    * fix:  uninstall primeflex primeicons primeng and from scss
    
    * fix: update format in all fil
    
    ---------
    
    Co-authored-by: Gustav Blomqvist <gustav.blomqvist@luminatorhost.onmicrosoft.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,30 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { SubtitleComponent } from './subtitle/subtitle.component';
 import { TitleComponent } from './titile/titile.component';
-import { QrCodeComponent } from './qr-code/qr-code.component';
 import { QRCodeModule } from 'angularx-qrcode';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
 import { HttpClientModule } from '@angular/common/http';
 import { TypeComponent } from './type/type.component';
 import { StopListComponent } from './stop-list/stop-list.component';
 import { ImageComponent } from './image/image.component';
-import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QrCodeComponent,
     SubtitleComponent,
     TitleComponent,
     TypeComponent,
     StopListComponent,
-    ImageComponent
+    ImageComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    ButtonModule,
     HttpClientModule,
     QRCodeModule,
-    CardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
