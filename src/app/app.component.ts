cfd31376e15630133c86d073e0bc0993dc6b15e8
KlaudiaLum <klaudia.wroblewska@luminator.com>
2024-05-06 09:21:07 +0200

    removed debugge code
    Feat/query parameters (#22)
    
    * added feature for black lisr
    
    * fix: bulid problem
    
    * fix: rebase two branches into one
    
    * fix: changed blacklist conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: ` <app-stop-list></app-stop-list> `,
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
